cb8094dc25f25243256b9a30994ed45269e86618
usadev1984 <chadodev84@proton.me>
2023-08-11 14:35:27 +0300

    change how systemConfig is generated

--- sqlite2json.py
+++ sqlite2json.py
@@ -18,16 +18,25 @@ with open(outfile, 'w') as o:
     o.writelines('')
 
 with open(infile, 'r') as f:
+    f.readline()
+    line = f.readline()
+    if search(r"\"system_config\"", line):
+        with open(outfile, 'a') as o:
+            o.writelines('{' + '\n')
+            o.writelines('\t"systemConfig":' + '\n')
+            o.writelines('\t{' + '\n')
+
+            f.readline()
+            f.readline()
+
+            line = f.readline()
+            s = search(r"(?P<ee>\".*\"): ?(?P<value>\".*\")", line).groupdict()
+            o.writelines('\t\t"motd": {}'.format(s['value']) + ',\n')
+            o.writelines('\t\t"tags": []\n')
+            o.writelines(f.readline()[:-1] + ',\n')
+            f.readline()
     while(1):
         line = f.readline()
-        if search(r"\"motd\"", line):
-            with open(outfile, 'a') as o:
-                if line[-1] == '\n':
-                    o.writelines(line[:-1] + ',\n')
-                else:
-                    o.writelines(line + ',\n')
-                o.writelines('\t"tags": []\n')
-            continue
         if not search(r"\"joined\"|\"left\"|lastActive|cooldownUntil|warnExpiry", line):
             with open(outfile, 'a') as o:
                 o.writelines(line)
@@ -36,7 +45,8 @@ with open(infile, 'r') as f:
             with open(outfile, 'a') as o:
                 o.writelines(line)
             continue
-        s = search(r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})\s+(?P<hour>\d{2}):(?P<min>\d{2}):(?P<sec>\d{2})", line).groupdict()
+        s = search(r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})\s+" + 
+                   "(?P<hour>\d{2}):(?P<min>\d{2}):(?P<sec>\d{2})", line).groupdict()
         #datetime()
         print(line)
         stamp = str(int(datetime(int(s['year']),
